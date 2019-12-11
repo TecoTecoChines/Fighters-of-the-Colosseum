e42cee2d3a5f5590ca7d1cc13b44651fd4b3ef6e
TecoTecoChines <50380037+TecoTecoChines@users.noreply.github.com>
2019-12-11 00:18:12 -0300

    TCC

---
+++ Assets/Scripts/Playable Character/Attack Detection/SpecialDetection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialDetection : MonoBehaviour
+{
+    GameObject special;
+    SpecialDamage attack;
+    Animator anim;
+    void Start(){
+        special = transform.parent.gameObject;
+        anim = special.GetComponent<Animator>();
+        attack = special.GetComponent<SpecialDamage>();
+    }
+    void OnTriggerEnter2D(Collider2D col){
+        if(col == null){
+            Debug.Log("wtf");
+        }
+        if(col.gameObject.tag == "Hurt Box"){
+//            Debug.Log(gameObject.name + "Did " + attack.attackInfo.damage + " damage and " + attack.attackInfo.hitstun + " hitstun");
+            col.gameObject.GetComponent<DamageDetection>().TakeDamage(attack.attackInfo);
+            StartCoroutine(Hitfreeze(attack.attackInfo.hitlag));
+        }
+    }
+
+    IEnumerator Hitfreeze(float timer){
+        anim.speed = 0;
+        yield return new WaitForSeconds(timer);
+        anim.speed = 1;
+    }
+}
---
+++ Assets/Scripts/Scriptable Objects/AttackData.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New AttackData", menuName = "Attack Data", order = 51)]
+public class AttackData : ScriptableObject
+{
+    public enum AttackType{
+        Normal = 0,
+        Skill = 1,
+        SpecialSkill = 2
+    }
+    public enum HitType{
+        directAttack = 0,
+        projectile = 1,
+        solidObject = 2
+    }   
+    [SerializeField]
+    Sprite icon;
+    [SerializeField]
+    string attackName = "";
+    [SerializeField]
+    public float cooldown;
+    [SerializeField]
+    public float manaCost;
+    [SerializeField]
+    public HitData[] hitData = new HitData[1];
+    [SerializeField]
+    public AttackType attackType;
+    [SerializeField]
+    public HitType hitType;
+    [SerializeField]
+    public MovementData movementData;
+
+    public Sprite Icon{
+        get{
+            return icon;
+        }
+    }
+
+    public string AttackName{
+        get{
+            return attackName;
+        }
+    }
+    public float HitNumber{
+        get{
+            return hitData.Length;
+        }
+    }
+    public float ManaCost{
+        get{
+            return manaCost;
+        }
+    }
+}
+
+[System.Serializable]
+public class HitData{
+    [SerializeField]
+    float manaGain;
+    [SerializeField]
+    int dmgMultiplier;
+    [SerializeField]
+    float knockback;
+    [SerializeField]
+    float knockup;
+    [SerializeField]
+    float hitstun;
+    [SerializeField]
+    float hitlag;
+
+    public float ManaGain{
+        get{
+            return manaGain;
+        }
+    }
+    public int DmgMultiplier{
+        set{
+            dmgMultiplier = value;
+        }
+        get{
+            return dmgMultiplier;
+        }
+    }
+
+    public float Knockback{
+        get{
+            return knockback;
+        }
+    }
+
+    public float Knockup{
+        get{
+            return knockup;
+        }
+    }
+
+    public float Hitstun{
+        get{
+            return hitstun;
+        }
+    }
+
+    public float Hitlag{
+        get{
+            return hitlag;
+        }
+    }
+}
